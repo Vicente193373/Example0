1f8671a7dc55708d8a1b99509c070e49a8864c96
mendlez12 <mendlez12@MENDLEZ-TP>
2025-02-06 16:27:07 -0700

    First Commit, added int x after Hello World print.

---
+++ src/main/java/com/mycompany/repositorypractice/RepositoryPractice.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorypractice;
+
+/**
+ *
+ * @author mendlez12
+ */
+public class RepositoryPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int x;
+    }
+}
